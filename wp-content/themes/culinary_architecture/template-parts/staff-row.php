b70db78b4273369ee5c113a6f330c96fd8835661
Derek <derek@Dereks-iMac.local>
2016-03-31 19:33:15 -0400

    client changes

--- wp-content/themes/culinary_architecture/template-parts/staff-row.php
+++ wp-content/themes/culinary_architecture/template-parts/staff-row.php
@@ -9,7 +9,27 @@
 		while ( $loop->have_posts() ) : $loop->the_post();
 		?>
 
-			<li><a href="<?php the_permalink(); ?>"><img src="<?php echo the_field('staff_image'); ?>" title="<?php the_title(); ?> Staff Image" alt="<?php the_title(); ?> Staff Image" class="circle-quote"></a><h3><a href="<?php the_permalink(); ?>"><?php echo the_field('staff_name'); ?></a></h3><p><?php echo the_field('staff_title'); ?></p></li>
+			<li>
+
+					<?php if (get_field('staff_name') != 'Josephine'): ?>
+
+					<a href="<?php the_permalink(); ?>">
+						<img src="<?php echo the_field('staff_image'); ?>" title="<?php the_title(); ?> Staff Image" alt="<?php the_title(); ?> Staff Image" class="circle-quote">
+					</a>
+
+					<h3><a href="<?php the_permalink(); ?>"><?php echo the_field('staff_name'); ?></a></h3><p><?php echo the_field('staff_title'); ?></p>
+
+					<?php else: ?>
+
+						<a href="javascript: void(0);">
+							<img src="<?php echo the_field('staff_image'); ?>" title="<?php the_title(); ?> Staff Image" alt="<?php the_title(); ?> Staff Image" class="circle-quote">
+						</a>
+
+						<h3><a href="javascript: void(0);"><?php echo the_field('staff_name'); ?></a></h3><p><?php echo the_field('staff_title'); ?></p>
+
+					<?php endif; ?>
+
+				</li>
 
 		<?php endwhile;?>
 	</ul>
